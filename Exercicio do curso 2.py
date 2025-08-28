527da311765ee77edf381caf5bdc3aabc5c8c0d3
saymonprote22 <saymonprote@gmail.com>
2025-08-28 11:21:38 -0300

    Add files via upload

---
+++ Exercicio do curso 2.py
@@ -0,0 +1,4 @@
+dia=int(input("Qual o dia que você nasceu? "))
+mes=int(input("Qual mês vocês nasceu? "))
+ano=int(input("Qual foi o ano que você nasceu? "))
+print("Voce nasceu no dia",dia,mes,ano)
---
+++ Exercicio do curso 3.py
@@ -0,0 +1,4 @@
+n1=int(input("digte um numero"))
+n2=int(input("digte outro numero"))
+calc=(n1+n2)
+print("a soma dos dois numeros deu",calc)
